927cdcd99a5f9c89edab58d21c68c686f0cb82fd
Nogeese <leon8326nogeese@outlook.com>
2025-01-27 20:43:03 +0000

    Create infostack-core-group.sh

---
+++ infostack-core-group.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# InfoStack Core Essensials Group Source for updates.
+
+# Basic Info
+INFOTYPE=group
+INFOGROUPNAME=infostack-core-group
+INFOLATESTVERSION=0.06
+
+# Sources settings
+INFOINSTALLABLE=true
+INFOSTACKSENABLE=true
+
+# Sources
+INFOSTACKSSOURCES=(
+
+"https://raw.githubusercontent.com/leon8326-nogeese/InfoKit/main/infostack.sh"
+"https://raw.githubusercontent.com/leon8326-nogeese/InfoKit/main/infostacks/infostack-core0/infostack.sh"
+
+)
+
