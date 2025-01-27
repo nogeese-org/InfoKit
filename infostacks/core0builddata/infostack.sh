cfc78a115b3095bf976f8ba2ae6361f5dcd7f1e4
Nogeese <leon8326nogeese@outlook.com>
2025-01-27 20:56:43 +0000

    Create infostack.sh

---
+++ infostacks/core0builddata/infostack.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# InfoStack Source
+
+# Basic Info
+INFOTYPE=software
+INFOSOFTWARENAME=org.neocities-leon8326.Nogeese.InfoStack.core0
+INFOLATESTVERSION=0
+
+# Sources settings
+INFOINSTALLABLE=false # Please install the core group to install this build.
+INFOREINDERENABLE=false
+INFOINSTALLERENABLE=false
+INFOREPOENABLE=false
+INFOPACKAGEENABLE=false
+
+# Dependencies
+DEPENDENCIESENABLE=false
