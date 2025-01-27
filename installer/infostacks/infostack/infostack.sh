97ebd4c4a0531c11daf776581d8c3d579bae1ca4
Nogeese <leon8326nogeese@outlook.com>
2025-01-27 19:29:38 +0000

    Create infostack.sh

---
+++ installer/infostacks/infostack/infostack.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# InfoKit Core Source for updates.
+
+# Basic Info
+INFOTYPE=software
+INFOSOFTWARENAME=infokit
+INFOLATESTVERSION=0.04
+
+# Sources settings
+INFOINSTALLABLE=true
+INFOREINDERENABLE=false
+INFOINSTALLERENABLE=false
+INFOREPOENABLE=false
+INFOPACKAGEENABLE=false
+
+# Sources
+INFOINSTALLERSOURCE=https://raw.githubusercontent.com/leon8326-nogeese/InfoKit/main/installer/syswideinstall.sh
+
+# Dependencies
+DEPENDENCIESENABLE=false
