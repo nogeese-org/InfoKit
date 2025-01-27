e737f43ca8f69c7f267939cf6a1bb82f541e2910
Nogeese <leon8326nogeese@outlook.com>
2025-01-27 16:09:01 +0000

    Create infostack.sh

---
+++ infostack.sh
@@ -0,0 +1,21 @@
+# InfoStack Core Script for updates.
+
+# Basic Info
+INFOTYPE=software
+INFOSOFTWARENAME=infostack
+INFOLATESTVERSION=0.01
+
+# Sources settings
+INFOINSTALLABLE=true
+INFOREINDERENABLE=false
+INFOINSTALLERENABLE=true
+INFOREPOENABLE=true
+INFOPACKAGEENABLE=false
+
+# Sources
+INFOINSTALLERSOURCE=https://raw.githubusercontent.com/leon8326-nogeese/InfoKit/main/installer/installer.sh
+INFOREPOMAIN=https://raw.githubusercontent.com/leon8326-nogeese/InfoKit/main/
+INFOREPOSOURCE=https://raw.githubusercontent.com/leon8326-nogeese/pacconf/main/infosource/
+
+# Dependencies
+DEPENDENCIESENABLE=false
