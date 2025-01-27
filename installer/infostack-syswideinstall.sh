c938d2899aac9e50219e3c62218f90db910d03a4
Nogeese <leon8326nogeese@outlook.com>
2025-01-27 19:27:39 +0000

    Update and rename syswideinstall.sh to infostack-syswideinstall.sh

---
+++ installer/infostack-syswideinstall.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -sSL https://raw.githubusercontent.com/leon8326-nogeese/InfoKit/main/installer/infostack-installer.sh >> infostack-installer.sh && sudo bash infostack-installer.sh --system && rm infostack-installer.sh
