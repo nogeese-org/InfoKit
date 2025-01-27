8d36885c59a6ec9d7e5f82b175afa6c26f1549a7
Nogeese <leon8326nogeese@outlook.com>
2025-01-27 20:45:30 +0000

    Update infostack.sh

--- infostack.sh
+++ infostack.sh
@@ -10,14 +10,12 @@ INFOLATESTVERSION=0.06
 # Sources settings
 INFOINSTALLABLE=true
 INFOREINDERENABLE=false
-INFOINSTALLERENABLE=false
-INFOREPOENABLE=true
+INFOINSTALLERENABLE=true
+INFOREPOENABLE=false
 INFOPACKAGEENABLE=false
 
 # Sources
-INFOINSTALLERSOURCE=https://raw.githubusercontent.com/leon8326-nogeese/InfoKit/main/installer/syswideinstall.sh
-INFOREPOMAIN=https://raw.githubusercontent.com/leon8326-nogeese/InfoKit/main/
-INFOREPOSOURCE=https://raw.githubusercontent.com/leon8326-nogeese/pacconf/main/infosource/
+INFOINSTALLERSOURCE=https://raw.githubusercontent.com/leon8326-nogeese/InfoKit/main/installer/infostack-core.sh
 
 # Dependencies
 DEPENDENCIESENABLE=false
