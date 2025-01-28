2c3a2344c05db49accc97c41f64ff96d309f23bf
Nogeese <leon8326nogeese@outlook.com>
2025-01-28 13:08:32 +0000

    Create infostack.sh

---
+++ infostacks/infostack-core/infostack.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# InfoStack Source
+
+# Basic Info
+INFOTYPE=software
+INFOSOFTWARENAME=infostack-core
+INFOLATESTVERSION=0
+
+# Sources settings
+INFOINSTALLABLE=true
+INFOREINDERENABLE=false
+INFOINSTALLERENABLE=true
+INFOREPOENABLE=false
+INFOPACKAGEENABLE=false
+
+# Sources
+INFOINSTALLERSOURCE=https://raw.githubusercontent.com/leon8326-nogeese/InfoKit/main/installer/infostack-core.sh
+
+# Dependencies
+DEPENDENCIESENABLE=false
