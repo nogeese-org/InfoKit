cb665e65565bce52863321f781f02a486fa7ab5b
Nogeese <leon8326nogeese@outlook.com>
2025-02-03 11:16:02 +0000

    Update install.sh

--- installer/install.sh
+++ installer/install.sh
@@ -6,6 +6,7 @@ echo "Please wait..."
 sleep 1
 echo "Installing core scripts..."
 sudo curl -sSL https://raw.githubusercontent.com/leon8326-nogeese/InfoKit/main/bin/infokit-softwaremanager >> /usr/bin/infokit-softwaremanager
+sudo chmod +x /usr/bin/infokit-softwaremanager
 echo "Installing software: infostack"
 sleep 2
 curl -sSL https://raw.githubusercontent.com/leon8326-nogeese/InfoKit/main/installer/infostack-syswideinstall.sh | sudo bash
